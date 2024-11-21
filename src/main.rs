88fc7173c3fff3278f1fb212e5ac4521e1df6dbc
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-11-21 18:04:17 +0900

    feat:style지정

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use iced::{
+    border::color,
     mouse,
     widget::{
-        button, canvas,
+        button,
         canvas::{
+            self,
             event::{self, Event},
             Canvas, Program,
         },
         checkbox, column, container, pick_list, text, text_input, Checkbox, Column, Container,
-        PickList, Space, Text,
+        PickList, Row, Space, Text,
     },
-    Color, Element, Font, Length, Pixels, Point, Rectangle, Size, Subscription, Theme,
+    Background, Border, Color, Element, Font, Length, Pixels, Point, Rectangle, Size, Subscription,
+    Theme,
 };
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 
 pub enum Message {
     ERROR,
@@ -25,7 +28,21 @@ impl Default for ERP {
 }
 impl ERP {
     pub fn view(&self) -> Element<Message> {
-        Column::new().into()
+        let top = Column::new().push(
+            Row::new()
+                .push(button(text("aa")).width(Length::FillPortion(1)))
+                .push(button(text("aa")).width(Length::FillPortion(1)))
+                .push(button(text("aa")).width(Length::FillPortion(1)))
+                .push(button(text("aa")).width(Length::FillPortion(1))),
+        );
+
+        let middle = Container::new(Space::new(10, 10)).style(|_state| container::Style {
+            // text_color: Some(Color::new(200., 1., 1., 1.)),
+            background: Some(Background::Color(Color::from_rgb(255., 0., 0.))),
+            border: Border::default(), // 이 부분은 다른 방식으로 수정해야 할 수 있습니다.
+            ..container::Style::default()
+        });
+        Column::new().push(top).push(middle).into()
     }
     pub fn update(&mut self, message: Message) {
         match message {
